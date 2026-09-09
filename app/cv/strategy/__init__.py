4b75d2f79ff5253da6f5010cf087088e779a18ee
Juan Manuel Torres <juan.manuel.torres@mi.unc.edu.ar>
2026-09-09 01:03:58 -0300

    feat: add CV strategy models

---
+++ app/cv/strategy/__init__.py
@@ -0,0 +1,13 @@
+from app.cv.strategy.models import (
+    STRATEGY_VERSION,
+    CoreMessage,
+    CVStrategy,
+    StrategyTrackConfig,
+)
+
+__all__ = [
+    "STRATEGY_VERSION",
+    "CoreMessage",
+    "CVStrategy",
+    "StrategyTrackConfig",
+]
