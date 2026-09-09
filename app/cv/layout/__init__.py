d844abb3709accd07cadca1640bc6d95d042a38f
Juan Manuel Torres <juan.manuel.torres@mi.unc.edu.ar>
2026-09-09 15:19:04 -0300

    feat: export CV layout profile API

---
+++ app/cv/layout/__init__.py
@@ -0,0 +1,8 @@
+from app.cv.layout.models import LAYOUT_PROFILE_VERSION, LayoutProfile
+from app.cv.layout.registry import load_layout_profiles
+
+__all__ = [
+    "LAYOUT_PROFILE_VERSION",
+    "LayoutProfile",
+    "load_layout_profiles",
+]
